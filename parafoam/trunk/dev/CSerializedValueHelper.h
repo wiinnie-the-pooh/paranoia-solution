5936d79ad97d74f86128d39bfc81d2dc61740713
alexey.petrov.nnov <alexey.petrov.nnov@cdfb17f8-2921-11df-9f4f-a5c41f8f774a>
2010-04-08 16:13:47 +0000

    fix compilation problem

--- parafoam/trunk/dev/CSerializedValueHelper.h
+++ parafoam/trunk/dev/CSerializedValueHelper.h
@@ -26,7 +26,7 @@
 
 
 //---------------------------------------------------------------------------
-#include "parallel/dev/CSerializedDataHolder.h"
+#include "parallel/dev/CSimpleValueHelper.h"
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
