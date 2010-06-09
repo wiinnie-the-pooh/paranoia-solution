f37935881d54a6b9bf45b4372103daa6563abe61
alexey.petrov.nnov <alexey.petrov.nnov@cdfb17f8-2921-11df-9f4f-a5c41f8f774a>
2010-06-09 16:08:04 +0000

    to start CORBA based implementation

---
+++ parafoam/branches/corba_r81/parallel/base/TSmartPtr.h
@@ -0,0 +1,104 @@
+// VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+// Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
+//
+// Author : Alexey PETROV
+//
+
+
+//---------------------------------------------------------------------------
+#ifndef TSmartPtr_h
+#define TSmartPtr_h
+
+
+//---------------------------------------------------------------------------
+#include "parallel/base/TGenericObj.h"
+
+#include <loki/SmartPtr.h>
+
+
+//---------------------------------------------------------------------------
+namespace parallel
+{
+  namespace base
+  {
+    //---------------------------------------------------------------------------
+    template< class P >
+    struct TOwnershipPolicy
+    {
+      TOwnershipPolicy()
+      {}
+      
+      template< class U >
+      TOwnershipPolicy( const TOwnershipPolicy< U >& )
+      {}
+
+      static P Clone( const P& val )
+      {
+        if ( val != 0 )
+          val->Register();
+        
+        return val;
+      }
+
+      static bool Release( const P& val )
+      {
+        if ( val != 0 )
+          val->Destroy();
+        
+        return false;
+      }
+      
+      enum { destructiveCopy = false };
+      
+      static void Swap( TOwnershipPolicy& )
+      {}
+    };
+
+
+    //---------------------------------------------------------------------------
+    template
+    <
+      typename T,
+      template <class> class OwnershipPolicy = TOwnershipPolicy,
+      class ConversionPolicy = Loki::DisallowConversion,
+      template <class> class CheckingPolicy = Loki::AssertCheck,
+      template <class> class StoragePolicy = Loki::DefaultSPStorage,
+      template<class> class ConstnessPolicy = LOKI_DEFAULT_CONSTNESS
+    >
+    struct SmartPtrDef
+    {
+      typedef Loki::SmartPtr
+      <
+        T,
+        OwnershipPolicy,
+        ConversionPolicy,
+        CheckingPolicy,
+        StoragePolicy,
+        ConstnessPolicy
+      >
+      type;
+    };
+
+
+    //---------------------------------------------------------------------------
+  }
+}
+
+
+//---------------------------------------------------------------------------
+#endif
---
+++ parafoam/branches/corba_r81/parallel/dev/CConvergencyCriteriaTask.h
@@ -0,0 +1,83 @@
+// VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+// Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
+//
+// Author : Alexey PETROV
+//
+
+
+//---------------------------------------------------------------------------
+#ifndef dev_CConvergencyCriteriaTask_h
+#define dev_CConvergencyCriteriaTask_h
+
+
+//---------------------------------------------------------------------------
+#include "parallel/dev/TTask.h"
+
+#include "parallel/dev/CPortHelper.h"
+
+#include "parallel/dev/CBoolPort.h"
+
+#include "parallel/dev/CDoublePort.h"
+
+
+//---------------------------------------------------------------------------
+namespace parallel
+{
+  namespace dev
+  {
+    //---------------------------------------------------------------------------
+    struct CConvergencyCriteriaTask : TTask
+    {
+      typedef base::SmartPtrDef< CConvergencyCriteriaTask >::type TPtr;
+ 
+      CConvergencyCriteriaTask();
+
+      ~CConvergencyCriteriaTask();
+
+      void setMaxResidual( double the_residual );
+      double getMaxResidual() const;
+      
+      void setMaxIterationNumber( int the_number );
+      int getMaxIterationNumber() const;
+      
+    protected:
+      CPortHelper< CDoublePort > m_residual_i;
+      CPortHelper< CBoolPort > m_stop_i;
+
+      CPortHelper< CBoolPort > m_finished_o;
+      CPortHelper< CBoolPort > m_result_o;
+
+    protected:
+      double maxResidual;
+      int maxIterationNumber;
+      int iterationCounter;
+
+    protected:
+      virtual void init();
+      virtual bool step();
+      virtual void destroy();
+    };
+
+
+    //-----------------------------------------------------------------------
+  }
+}
+
+
+//---------------------------------------------------------------------------
+#endif
