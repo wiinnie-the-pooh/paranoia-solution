8e2a195048215ded8e74a47a769b33a9bec63658
alexey.petrov.nnov <alexey.petrov.nnov@cdfb17f8-2921-11df-9f4f-a5c41f8f774a>
2010-06-16 10:57:52 +0000

    The draft implementation of TaskFactory IDL interface

--- parafoam/branches/corba_r81/parallel/corba/eg3_clt.cc
+++ parafoam/branches/corba_r81/parallel/corba/eg3_clt.cc
@@ -1,147 +1,126 @@
-// eg3_clt.cc - This is the source code of example 3 used in Chapter 2
-//              "The Basics" of the omniORB user guide.
+// VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+// Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
 //
-//              This is the client. It uses the COSS naming service
-//              to obtain the object reference.
-//
-// Usage: eg3_clt
-//
-//
-//        On startup, the client lookup the object reference from the
-//        COS naming service.
-//
-//        The name which the object is bound to is as follows:
-//              root  [context]
-//               |
-//              text  [context] kind [my_context]
-//               |
-//              Echo  [object]  kind [Object]
+// Author : Alexey PETROV
 //
 
-#include <echo.hh>
 
-#ifdef HAVE_STD
-#  include <iostream>
-   using namespace std;
-#else
-#  include <iostream.h>
-#endif
+//---------------------------------------------------------------------------
+#include <TaskFactory.hh>
 
-static CORBA::Object_ptr getObjectReference(CORBA::ORB_ptr orb);
+#include <iostream>
+using namespace std;
 
-static void hello(Echo_ptr e)
-{
-  if( CORBA::is_nil(e) ) {
-    cerr << "hello: The object reference is nil!\n" << endl;
-    return;
-  }
 
-  CORBA::String_var src = (const char*) "Hello!";
+//---------------------------------------------------------------------------
+static CORBA::Object_ptr getObjectReference( CORBA::ORB_ptr orb );
 
-  CORBA::String_var dest = e->echoString(src);
 
-  cerr << "I said, \"" << (char*)src << "\"." << endl
-       << "The Echo object replied, \"" << (char*)dest <<"\"." << endl;
-}
-
-//////////////////////////////////////////////////////////////////////
-
-int
-main (int argc, char **argv) 
+//---------------------------------------------------------------------------
+int main( int argc, char **argv ) 
 {
   try {
-    CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
+    CORBA::ORB_var orb = CORBA::ORB_init( argc, argv );
 
-    CORBA::Object_var obj = getObjectReference(orb);
+    CORBA::Object_var obj = getObjectReference( orb );
 
-    Echo_var echoref = Echo::_narrow(obj);
+    TaskFactory_var a_task_factory_ref = TaskFactory::_narrow( obj );
 
-    for (CORBA::ULong count=0; count < 10; count++)
-      hello(echoref);
+    Task_var a_task = a_task_factory_ref->create();
 
     orb->destroy();
   }
-  catch(CORBA::TRANSIENT&) {
+  catch( CORBA::TRANSIENT& ) {
     cerr << "Caught system exception TRANSIENT -- unable to contact the "
          << "server." << endl;
   }
-  catch(CORBA::SystemException& ex) {
+  catch( CORBA::SystemException& ex ) {
     cerr << "Caught a CORBA::" << ex._name() << endl;
   }
-  catch(CORBA::Exception& ex) {
+  catch( CORBA::Exception& ex ) {
     cerr << "Caught CORBA::Exception: " << ex._name() << endl;
   }
-  catch(omniORB::fatalException& fe) {
+  catch( omniORB::fatalException& fe ) {
     cerr << "Caught omniORB::fatalException:" << endl;
     cerr << "  file: " << fe.file() << endl;
     cerr << "  line: " << fe.line() << endl;
     cerr << "  mesg: " << fe.errmsg() << endl;
   }
+
   return 0;
 }
 
-//////////////////////////////////////////////////////////////////////
 
-static CORBA::Object_ptr
-getObjectReference(CORBA::ORB_ptr orb)
+//---------------------------------------------------------------------------
+static CORBA::Object_ptr getObjectReference( CORBA::ORB_ptr orb )
 {
   CosNaming::NamingContext_var rootContext;
   
   try {
     // Obtain a reference to the root context of the Name service:
     CORBA::Object_var obj;
-    obj = orb->resolve_initial_references("NameService");
-
-    // Narrow the reference returned.
-    rootContext = CosNaming::NamingContext::_narrow(obj);
-    if( CORBA::is_nil(rootContext) ) {
+    obj = orb->resolve_initial_references( "NameService" );
+    rootContext = CosNaming::NamingContext::_narrow( obj );
+    if( CORBA::is_nil( rootContext ) ) {
       cerr << "Failed to narrow the root naming context." << endl;
       return CORBA::Object::_nil();
     }
   }
-  catch (CORBA::NO_RESOURCES&) {
+  catch ( CORBA::NO_RESOURCES& ) {
     cerr << "Caught NO_RESOURCES exception. You must configure omniORB "
 	 << "with the location" << endl
 	 << "of the naming service." << endl;
+
     return 0;
   }
-  catch(CORBA::ORB::InvalidName& ex) {
+  catch( CORBA::ORB::InvalidName& ex ) {
     // This should not happen!
     cerr << "Service required is invalid [does not exist]." << endl;
+
     return CORBA::Object::_nil();
   }
 
   // Create a name object, containing the name test/context:
   CosNaming::Name name;
-  name.length(2);
+  name.length( 2 );
 
-  name[0].id   = (const char*) "test";       // string copied
-  name[0].kind = (const char*) "my_context"; // string copied
-  name[1].id   = (const char*) "Echo";
-  name[1].kind = (const char*) "Object";
-  // Note on kind: The kind field is used to indicate the type
-  // of the object. This is to avoid conventions such as that used
-  // by files (name.type -- e.g. test.ps = postscript etc.)
+  name[ 0 ].id   = (const char*) "TaskFactory";
+  name[ 0 ].kind = (const char*) "paraFoam";
+
+  name[ 1 ].id   = (const char*) "XXX";
+  name[ 1 ].kind = (const char*) "object";
 
   try {
-    // Resolve the name to an object reference.
-    return rootContext->resolve(name);
+    return rootContext->resolve( name );
   }
-  catch(CosNaming::NamingContext::NotFound& ex) {
-    // This exception is thrown if any of the components of the
-    // path [contexts or the object] aren't found:
+  catch( CosNaming::NamingContext::NotFound& ex ) {
     cerr << "Context not found." << endl;
   }
-  catch(CORBA::TRANSIENT& ex) {
+  catch( CORBA::TRANSIENT& ex ) {
     cerr << "Caught system exception TRANSIENT -- unable to contact the "
          << "naming service." << endl
 	 << "Make sure the naming server is running and that omniORB is "
 	 << "configured correctly." << endl;
-
   }
-  catch(CORBA::SystemException& ex) {
+  catch( CORBA::SystemException& ex ) {
     cerr << "Caught a CORBA::" << ex._name()
 	 << " while using the naming service." << endl;
+
     return 0;
   }
 
